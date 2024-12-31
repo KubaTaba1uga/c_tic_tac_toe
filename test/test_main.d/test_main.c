3fc0d9342a1abbc4a395053c4f40bc05dddb648f
KubaTaba1uga <luna7337anul@proton.me>
2024-12-31 20:47:48 +0000

    fix tests

--- test/test_main.d/test_main.c
+++ test/test_main.d/test_main.c
@@ -1,19 +1,44 @@
+#include <signal.h>
+#include <stdio.h>
+
 #include <unity.h>
 
 #include "game/game.h"
 #include "input/input.h"
 #include "main.h"
 
-int input_wait_mock(void) { return 0; };
+static void suppress_signal(int sig) {
+  struct sigaction sa;
+  sa.sa_handler = SIG_IGN; // Ignore the signal
+  sa.sa_flags = 0;
+  sigemptyset(&sa.sa_mask);
+
+  if (sigaction(sig, &sa, NULL) == -1) {
+    perror("sigaction");
+  }
+}
+
+static void restore_signal(int sig) {
+  struct sigaction sa;
+  sa.sa_handler = SIG_DFL; // Restore the default handler
+  sa.sa_flags = 0;
+  sigemptyset(&sa.sa_mask);
+
+  if (sigaction(sig, &sa, NULL) == -1) {
+    perror("sigaction");
+  }
+}
+
+static int input_wait_mock(void) { return 0; };
 
 void setUp() {
   struct InputOps *input_ops = get_input_ops();
-  /* struct InitOps *init_ops = get_init_ops();   */
-  // Disable game init and destroy
-  /* init_game_reg.init = NULL; */
-  /* init_game_reg.destroy = NULL; */
-
   input_ops->wait = input_wait_mock;
+  suppress_signal(SIGUSR1);
+}
+
+void tearDown() {
+  restore_signal(SIGUSR1); // Restore default handling of SIGUSR1
 }
 
 void test_main(void) {
