88be96f2f8720f15d8da36a7513c3a000b2c46e2
KubaTaba1uga <luna7337anul@proton.me>
2025-01-01 12:47:49 +0000

    finish up example

--- docs/print_cli_example.c
+++ docs/print_cli_example.c
@@ -1,45 +1,42 @@
 #include <stddef.h>
 #include <stdio.h>
+#include <string.h>
 
-/* #define RESET_COLOR "\033[0m" */
-/* #define COLOR_RED "\033[31m" */
-/* #define COLOR_GREEN "\033[32m" */
-/* #define COLOR_BLUE "\033[34m" */
-/* #define COLOR_YELLOW "\033[33m" */
-/* #define COLOR_CYAN "\033[36m" */
-/* #define COLOR_MAGENTA "\033[35m" */
+// Define ANSI escape codes for background colors
+#define RESET_COLOR "\033[0m"
+#define COLOR_RED "\033[31m"
+#define COLOR_GREEN "\033[32m"
+#define COLOR_BLUE "\033[34m"
+#define COLOR_YELLOW "\033[33m"
+#define COLOR_CYAN "\033[36m"
+#define COLOR_MAGENTA "\033[35m"
+#define BG_RED "\033[41m"
+#define BG_GREEN "\033[42m"
+#define BG_BLUE "\033[44m"
+#define BG_YELLOW "\033[43m"
+#define BG_CYAN "\033[46m"
+#define BG_MAGENTA "\033[45m"
 
-// Array of color strings
-/* const int max_x = 3; */
-/* const int max_y = 3; */
+#define MAX_X 3
+#define MAX_Y 3
 
-/* const char *colors[max_y][max_x] = {{COLOR_RED, COLOR_GREEN, COLOR_BLUE}, */
-/*                                     {COLOR_YELLOW, COLOR_CYAN,
- * COLOR_MAGENTA}}; */
+const char *colors[MAX_Y][MAX_X] = {
+    {BG_RED, "", ""}, {"", "", ""}, {"", "", ""}};
 
 int main(void) {
-  const int max_x = 3;
-  const int max_y = 3;
 
-  for (size_t index_y = 0; index_y < max_y; index_y++) {
-    for (size_t index_x = 0; index_x < max_x; index_x++) {
-      char *str_to_print;
+  for (size_t index_y = 0; index_y < MAX_Y; index_y++) {
+    for (size_t index_x = 0; index_x < MAX_X; index_x++) {
+      char *delimeter_to_print;
 
-      if (index_y != max_y - 1) {
-        str_to_print = "_|";
+      delimeter_to_print = "|";
 
-        if (index_x == max_x - 1) {
-          str_to_print = "_\n";
-        }
-      } else {
-        str_to_print = " |";
-
-        if (index_x == max_x - 1) {
-          str_to_print = " \n";
-        }
+      if (index_x == MAX_X - 1) {
+        delimeter_to_print = "\n";
       }
 
-      printf("%s", str_to_print);
+      printf("%s %s%s", colors[index_y][index_x], RESET_COLOR,
+             delimeter_to_print);
     }
   }
 
